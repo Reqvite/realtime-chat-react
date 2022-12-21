3739627235e4a984c1468f8e25cc24db479cfa61
Reqvite <reqvite2233@gmail.com>
2022-12-21 13:00:09 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter basename="/react-realtime-chat/">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </ChakraProvider>
