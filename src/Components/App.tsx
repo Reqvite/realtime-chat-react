74c66c54b56f180900901201346871708512280c
Reqvite <reqvite2233@gmail.com>
2022-12-23 18:08:51 +0200

    Update App.tsx

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -14,6 +14,7 @@ import { auth } from "Service/firebaseAuth";
 const App: React.FC = () => {
   const [user, loading] = useAuthState(auth);
 
+  console.log(user);
   return loading ? (
     <Text>Loading</Text>
   ) : (
